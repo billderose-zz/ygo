7f55c8f6345ba28a2d3f7dda7501fc69dbda6fde
Bill DeRose <bill.derose11@gmail.com>
2013-11-05 22:03:40 -0800

    Nightly update

--- src/main.go
+++ src/main.go
@@ -6,9 +6,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-type business struct {
+type Business struct {
 	Business_id, Name, Full_address, City, State, Type string
 	Neighborhoods, Categories                          []string
 	Reviwew_count                                      int
@@ -16,30 +17,40 @@ type business struct {
 	Open                                               bool
 }
 
+func (b Business) String() string {
+	if len(b.Categories) != 0 {
+		return fmt.Sprintf("%f\t%f\t%s\n", b.Latitude, b.Longitude, strings.Replace(b.Categories[0], ",", "", -1))
+	}
+	return fmt.Sprintf("%f\t%f\t%s\n", b.Latitude, b.Longitude, "N/A")
 
-
-func (b business) String() string {
-	return fmt.Sprintf("%s\n %s\n", b.Name, b.Full_address)
 }
 
-var data = flag.String("source", "/Users/Bill/Downloads/yelp/yelp_academic_dataset_business.json", "Data to read")
-
 func main() {
+	data := flag.String("source", "", "Input data")
+	flag.Parse()
+
 	file, err := os.Open(*data)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	writeFile, err := os.Create("yelps.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	w := bufio.NewWriter(writeFile)
+	w.WriteString("lat\tlon\tcat\n")
 	r := bufio.NewReader(file)
 
 	for {
-		b := &business{}
+		b := &Business{}
 		if s, err := r.ReadSlice('\n'); err != nil {
 			break
 		} else if err := json.Unmarshal(s, &b); err != nil {
 			panic(err)
 		}
-		fmt.Println(b)
+		w.WriteString(b.String())
 	}
 }
