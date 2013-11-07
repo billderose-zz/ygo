4aadca5a29826b3896978c95b856753345c13d11
Bill DeRose <bill.derose11@gmail.com>
2013-11-06 21:38:43 -0800

    Delete mapper.R

--- src/mapper.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-library(ggmap)
-args <- commandArgs(TRUE)
-yelp <- read.delim(args[1], na.strings="N/A")
-az <- get_map("Pheonix, Arizona, United States", maptype="roadmap", zoom = 10)
-ggmap(az, extent='device', legend='none') + 
-      geom_point(aes(x=lon, y=lat), data=subset(yelp, cat == "Food"), na.rm=TRUE) 
